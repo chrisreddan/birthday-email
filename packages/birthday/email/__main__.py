3b764dccb0bb8e9ebcee0792c64a0dd2ac150ef0
Chris <chris@astorialabs.com>
2025-08-16 01:31:52 -0400

    Switched from sendgrid to postmark for emails

--- packages/birthday/email/__main__.py
+++ packages/birthday/email/__main__.py
@@ -1,57 +1,39 @@
-import logging
 import os
+import logging
 from datetime import datetime
 
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from postmarker.core import PostmarkClient
 
 
-def send_mail(message: Mail) -> None:
-    try:
-        sendgrid_key = os.getenv("SENDGRID_API_KEY")
-        sg = SendGridAPIClient(sendgrid_key)
-        response = sg.send(message)
-        logging.info(response)
-    except Exception as e:
-        logging.error(f"Error running process: {e}")
-
-
-def build_mail(people: iter) -> tuple[bool, Mail]:
-    email_from = os.getenv("EMAIL_FROM")
+def main(args):
+    current_date = datetime.now()
     email_to = []
     email_body = ""
-    has_birthday = False
+    is_birthday = False
+    people = args.get("people")
 
     for person in people:
         if person['is_active']:
             name = person['name']
             dob = str(person['dob'])
             birthday_date = datetime.strptime(dob, "%Y-%m-%d")
-            current_date = datetime.now()
-
+            if birthday_date.month == current_date.month and birthday_date.day == current_date.day:
+                is_birthday = True
+                age = current_date.year - birthday_date.year
+                email_body += f"{name} was born on {dob} and is {age} years old today. Happy email!<br><br>"
             if person['email'] is not None:
                 email_to.append(person['email'])
 
-            if birthday_date.month == current_date.month and birthday_date.day == current_date.day:
-                has_birthday = True
-                age = current_date.year - birthday_date.year
-                email_body += f"{name} was born on {dob} and is {age} years old today. Happy birthday!<br><br>"
-
     email_body += "-- This email was automatically sent because this person has a terrible memory -- "
 
-    mail = Mail(
-        from_email=email_from,
-        to_emails=email_to,
-        subject='Happy Birthday!',
-        html_content=email_body
-    )
-
-    return has_birthday, mail
-
-
-def main(args):
-    people = args.get("people")
-    has_birthday, mail = build_mail(people)
-
-    if has_birthday:
-        send_mail(mail)
+    if is_birthday:
+        try:
+            email_from = os.getenv("EMAIL_FROM")
+            api_token = os.getenv("POSTMARK_API_TOKEN")
+            logging.error(f"Sending email to {email_to}")
+            logging.error(f"Token = {api_token}")
+            postmark = PostmarkClient(api_token)
+            postmark.emails.send(From=email_from, To=email_to, Subject='Happy Birthday!', HtmlBody=email_body)
+            logging.info("Email sent")
+        except Exception as e:
+            logging.error(f"Error running process: {e}")
